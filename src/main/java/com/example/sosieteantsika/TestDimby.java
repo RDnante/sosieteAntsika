86209ca6e8a0531c58efe3c044826b55c3fce773
RDnante <119560912+RDnante@users.noreply.github.com>
2023-12-13 10:45:59 +0600

    login

--- src/main/java/com/example/sosieteantsika/TestDimby.java
+++ src/main/java/com/example/sosieteantsika/TestDimby.java
@@ -1,6 +1,7 @@
 package com.example.sosieteantsika;
 
 import com.example.sosieteantsika.connection.Connect;
+import com.example.sosieteantsika.model.Fournisseur;
 import com.example.sosieteantsika.model.Login;
 
 import java.sql.Connection;
@@ -12,7 +13,9 @@ public class TestDimby {
 //            Connect connect = new Connect();
 //            Connection connection = connect.connecter();
             Login test = l.login("RH123");
-            System.out.println(test.getId_service());
+            Fournisseur f = l.loginFournisseur("jumbo","1234");
+            System.out.println(f.getNom());
+//            System.out.println(test.getId_service());
         }catch (Exception e) {
             System.out.println(e.getMessage());
         }
